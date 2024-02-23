d29ac7e1fa0b9f82281dc87fd69c6977b4923618
Lior <lior-katz@campus.technion.ac.il>
2024-02-23 19:59:07 +0200

    Add tests for insert, remove, find, select_rank, get_rank, min, max

--- AVLTreeTest.cpp
+++ AVLTreeTest.cpp
@@ -2,17 +2,58 @@
 // Created by Lior on 23/02/2024.
 //
 
+#include "../wet1util.h"
 #include "gtest/gtest.h"
 #include "../AVL_Tree.h"
-TEST(AVLTreeTest, Constructor)
+
+#define SUCCESS StatusType::SUCCESS
+#define FAILURE StatusType::FAILURE
+#define SUITE AVLTreeTest
+
+class AVLTreeFixture : public ::testing::Test
+{
+protected:
+	AVL_Tree<int, int> avlTree = AVL_Tree<int, int>();
+	
+	std::vector<Pair<int, int>> vec = {{0, 5}, {1, 70}, {2, 70}, {4, 70}, {5, 70}, {6, 70}, {7, 70}, {8, 70}, {10, 50},
+									   {11, 30}, {30, 13}};
+	
+	std::string str;
+	
+	int size = vec.size();
+	
+	void SetUp() override
+	{
+		// Insert some elements into the AVL tree
+		avlTree.insert(10, 50);
+		avlTree.insert(11, 30);
+		avlTree.insert(0, 5);
+		avlTree.insert(30, 13);
+		avlTree.insert(1, 70);
+		avlTree.insert(2, 70);
+		avlTree.insert(4, 70);
+		avlTree.insert(5, 70);
+		avlTree.insert(6, 70);
+		avlTree.insert(7, 70);
+		avlTree.insert(8, 70);
+		
+		for (const auto& p : vec)
+		{
+			str += "(" + std::to_string(p.get_first()) + ", " + std::to_string(p.get_second()) + ")\n";
+		}
+	}
+};
+
+TEST(SUITE, Constructor)
 {
 	AVL_Tree<int, int> tree = AVL_Tree<int, int>();
+	ASSERT_TRUE(tree.is_valid());
 	EXPECT_EQ(tree.get_size(), 0);
-	EXPECT_EQ(tree.get_max().status(), StatusType::FAILURE);
-	EXPECT_EQ(tree.get_min().status(), StatusType::FAILURE);
-	EXPECT_EQ(tree.find(0).status(), StatusType::FAILURE);
+	EXPECT_EQ(tree.get_max().status(), FAILURE);
+	EXPECT_EQ(tree.get_min().status(), FAILURE);
+	EXPECT_EQ(tree.find(0).status(), FAILURE);
 	EXPECT_EQ(tree.get_rank(0), 0);
-	EXPECT_EQ(tree.select_rank(1).status(), StatusType::FAILURE);
+	EXPECT_EQ(tree.select_rank(1).status(), FAILURE);
 	std::vector<Pair<int, int>> res = std::vector<Pair<int, int>>();
 	EXPECT_EQ(tree.to_vec(), res);
 	std::stringstream ss;
@@ -20,7 +61,163 @@ TEST(AVLTreeTest, Constructor)
 	EXPECT_EQ(ss.str(), "");
 }
 
-TEST(AVLTreeTest, Insert)
+TEST_F(AVLTreeFixture, Size)
 {
-
+	EXPECT_EQ(avlTree.get_size(), size);
 }
+
+TEST(SUITE, SizeEmptyTree)
+{
+	auto tree = AVL_Tree<int, int>();
+	EXPECT_EQ(tree.get_size(), 0);
+}
+
+TEST(SUITE, Insert)
+{
+	AVL_Tree<int, int> tree = AVL_Tree<int, int>();
+	EXPECT_EQ(tree.insert(1, 1), SUCCESS);
+	EXPECT_EQ(tree.insert(2, 2), SUCCESS);
+	EXPECT_EQ(tree.insert(3, 3), SUCCESS);
+	EXPECT_EQ(tree.insert(4, 4), SUCCESS);
+	EXPECT_EQ(tree.insert(5, 5), SUCCESS);
+	EXPECT_EQ(tree.insert(6, 6), SUCCESS);
+	ASSERT_TRUE(tree.is_valid());
+	EXPECT_EQ(tree.get_size(), 6);
+	std::vector<Pair<int, int>> res = {{1, 1},
+									   {2, 2},
+									   {3, 3},
+									   {4, 4},
+									   {5, 5},
+									   {6, 6}};
+	EXPECT_EQ(tree.to_vec(), res);
+	std::stringstream ss;
+	tree.inorder(ss);
+	EXPECT_EQ(ss.str(), "(1, 1)\n(2, 2)\n(3, 3)\n(4, 4)\n(5, 5)\n(6, 6)\n");
+}
+
+TEST(SUITE, Insert_Duplicated)
+{
+	AVL_Tree<int, int> tree = AVL_Tree<int, int>();
+	EXPECT_EQ(tree.insert(1, 1), SUCCESS);
+	EXPECT_EQ(tree.insert(2, 2), SUCCESS);
+	EXPECT_EQ(tree.insert(3, 3), SUCCESS);
+	EXPECT_EQ(tree.insert(4, 4), SUCCESS);
+	EXPECT_EQ(tree.insert(5, 5), SUCCESS);
+	EXPECT_EQ(tree.insert(6, 6), SUCCESS);
+	ASSERT_TRUE(tree.is_valid());
+	EXPECT_EQ(tree.get_size(), 6);
+	std::vector<Pair<int, int>> res = {Pair<int, int>(1, 1), Pair<int, int>(2, 2), Pair<int, int>(3, 3),
+									   Pair<int, int>(4, 4), Pair<int, int>(5, 5), Pair<int, int>(6, 6)};
+	EXPECT_EQ(tree.to_vec(), res);
+	std::stringstream ss;
+	tree.inorder(ss);
+	EXPECT_EQ(ss.str(), "(1, 1)\n(2, 2)\n(3, 3)\n(4, 4)\n(5, 5)\n(6, 6)\n");
+	
+	EXPECT_EQ(tree.insert(2, 3), FAILURE);
+	ASSERT_TRUE(tree.is_valid());
+	EXPECT_EQ(tree.get_size(), 6);
+	EXPECT_EQ(tree.to_vec(), res);
+	ss.str(std::string());
+	tree.inorder(ss);
+	EXPECT_EQ(ss.str(), "(1, 1)\n(2, 2)\n(3, 3)\n(4, 4)\n(5, 5)\n(6, 6)\n");
+}
+
+TEST_F(AVLTreeFixture, Find)
+{
+	for (auto pair : vec)
+	{
+		auto res = avlTree.find(pair.get_first());
+		EXPECT_EQ(res.status(), SUCCESS);
+		EXPECT_EQ(res.ans(), pair.get_second());
+	}
+}
+
+TEST_F(AVLTreeFixture, FindNonExistent)
+{
+	EXPECT_EQ(avlTree.find(-1).status(), FAILURE);
+}
+
+TEST(SUITE, FindInEmptyTree)
+{
+	auto tree = AVL_Tree<int, int>();
+	EXPECT_EQ(tree.find(0).status(), FAILURE);
+}
+
+TEST_F(AVLTreeFixture, MinMax)
+{
+	// Get the minimum element
+	EXPECT_EQ(avlTree.get_min().status(), SUCCESS);
+	EXPECT_EQ(avlTree.get_min().ans(), 5);
+	
+	// Get the maximum element
+	EXPECT_EQ(avlTree.get_max().status(), SUCCESS);
+	EXPECT_EQ(avlTree.get_max().ans(), 13);
+}
+
+TEST_F(AVLTreeFixture, MinMaxEmptyTree)
+{
+	auto tree = AVL_Tree<int, int>();
+	EXPECT_EQ(tree.get_min().status(), FAILURE);
+	EXPECT_EQ(tree.get_max().status(), FAILURE);
+}
+
+TEST_F(AVLTreeFixture, SelectRank)
+{
+	for (int i = 1; i < vec.size(); ++i)
+	{
+		auto res = avlTree.select_rank(i);
+		EXPECT_EQ(res.status(), SUCCESS);
+		EXPECT_EQ(res.ans(), vec[i - 1].get_second());
+	}
+}
+
+TEST_F(AVLTreeFixture, SelectRankOutsideBounds)
+{
+	EXPECT_EQ(avlTree.select_rank(12).status(), FAILURE);
+	EXPECT_EQ(avlTree.select_rank(0).status(), FAILURE);
+	EXPECT_EQ(avlTree.select_rank(-1).status(), FAILURE);
+	EXPECT_EQ(avlTree.select_rank(15).status(), FAILURE);
+}
+
+TEST(SUITE, SelectRankEmptyTree)
+{
+	auto tree = AVL_Tree<int, int>();
+	EXPECT_EQ(tree.select_rank(0).status(), FAILURE);
+	EXPECT_EQ(tree.select_rank(1).status(), FAILURE);
+}
+
+TEST_F(AVLTreeFixture, GetRank)
+{
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		EXPECT_EQ(avlTree.get_rank(vec[i].get_first()), i+1);
+	}
+}
+
+TEST_F(AVLTreeFixture, GetRankNonExistent)
+{
+	EXPECT_EQ(avlTree.get_rank(-1), 0);
+}
+
+TEST(SUITE, GetRankEmptyTree)
+{
+	auto tree = AVL_Tree<int, int>();
+	EXPECT_EQ(tree.get_rank(0), 0);
+	EXPECT_EQ(tree.get_rank(1), 0);
+}
+
+TEST_F(AVLTreeFixture, Remove)
+{
+	for(auto pair : vec)
+	{
+		EXPECT_EQ(avlTree.remove(pair.get_first()), SUCCESS);
+		ASSERT_TRUE(avlTree.is_valid());
+		--size;
+		EXPECT_EQ(avlTree.get_size(), size);
+		EXPECT_EQ(avlTree.find(pair.get_first()).status(), FAILURE);
+	}
+	ASSERT_EQ(avlTree.get_size(), 0);
+	ASSERT_TRUE(avlTree.is_valid());
+}
+
+
